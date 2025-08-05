afe7704ee091f3bb076109751eb5ae945b2efaa6
Niklas Hempel <niklashempel9@gmail.com>
2025-08-05 17:09:08 +0200

    Remove ExternalId property from TeamDto

--- apps/api/DTOs/TeamDto.cs
+++ apps/api/DTOs/TeamDto.cs
@@ -3,7 +3,6 @@ namespace Api.DTOs;
 public class TeamDto
 {
     public int Id { get; set; }
-    public string? ExternalId { get; set; }
     public string? Name { get; set; }
     public int? ClubId { get; set; }
     public string? ClubName { get; set; }
